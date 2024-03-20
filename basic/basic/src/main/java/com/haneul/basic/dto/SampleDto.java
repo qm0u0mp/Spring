fdd4b47f6b95b4519338e3fa08cf95b80b914e73
qm0u0mp <99dngksmf@gmail.com>
2024-03-20 17:12:36 +0900

    Controller

---
+++ basic/basic/src/main/java/com/haneul/basic/dto/SampleDto.java
@@ -0,0 +1,17 @@
+package com.haneul.basic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SampleDto {
+    private String userId;
+    private String userPassword;
+}
