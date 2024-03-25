261e734d2e76216a838b4241cf85135c9c33b427
qm0u0mp <99dngksmf@gmail.com>
2024-03-25 17:13:46 +0900

    service

--- basic/basic/src/main/java/com/haneul/basic/controller/BasicController.java
+++ basic/basic/src/main/java/com/haneul/basic/controller/BasicController.java
@@ -1,5 +1,6 @@
 package com.haneul.basic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 // - 클라이언트의 요청을 받고 해당 요청에 대한 응답을 클라이언트에게 돌려주는 영역
 // - 각 요청에 해당하는 URL 메서드를 작성하는 영역
 
+import com.haneul.basic.service.BasicService;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 // @RestController : JSON 형태의 Response Body를 반환하는 Controller임을 명시
 // @RestController = @Controller + @ResponseBody
 @RestController
@@ -20,7 +26,30 @@ import org.springframework.web.bind.annotation.RestController;
 // @RequestMapping(value="/main", method={RequestMethod.GET})
 // HTTP GET localhost:4000/main/**
 @RequestMapping("/main")
+// @AllArgsConstructor
+// final로 선언된 필드 변수에 대해서 생성자를 자동으로 작성함
+@RequiredArgsConstructor
 public class BasicController {
+    // private BasicService service;
+
+    // 의존성 역전 (DI)
+    // - 해당 클래스에 필요한 의존성을 외부(클래스의 인스턴스를 생성하는 위치)에서 주입
+    // - 생정자를 통한 의존성 주입, setter 메서드를 통한 의존성 주입, 필드를 통한 의존성 주입
+    // - 생정자를 통한 의존성 주입을 spring 에서 권장 (의존성이 주입되지 않은 경우가 존재할 수 없기 때문)
+    // - Spring Framework에서 의존성 주입은 제어의 역전(IoC)를 통해서 Spring Framework가 진행
+    // (@Component를 사용해서 Spring Bean으로 등록된 Class에 한해서)
+
+    // @Autowired : 등록된 Spring Bean을 제어의 역전을 통해서 의존성을 주입하는 방법
+    // 단, 생성자를 통한 의존성 주입에는 @Autowired를 생략해도 됨
+    // public BasicController(BasicService service) {
+    // this.service = service;
+    // }
+
+    // 의존성 주입 시 클래스로 직접 참조변수를 만들지 않고 인터페이스로 간접적으로 만드는 이유
+    // - 고수준의 모듈에서 저수준의 모듈을 직접 참조하지 않고 추상화를 통해 간접 참조함으로써 각 모듈간의 결합도를 낮춤
+    // => 코드의 재사용성 향상
+    // => 유지보수성 향상
+    private final BasicService service;
 
     // HTTP GET localhost:4000/main/
     @RequestMapping(value = "/", method = { RequestMethod.GET })
